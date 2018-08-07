25dd38b3662d10201968cc0f62aad382a34fe90a
Chandler Fang <chandlerfang@gmail.com>
2018-08-08 00:00:02 +1000

    Add ResizableArray and LinkedListGeneric.

--- Algorithms-Csharp/array/ResizableArray.cs
+++ Algorithms-Csharp/array/ResizableArray.cs
@@ -1,15 +1,60 @@
 ﻿namespace Algorithms_Csharp.array
 {
+    using System;
+
     class ResizableArray
     {
         private int[] items;
         private int size;
+
+        int[] _oldArray = new int[10];
+
+        int[] _newArray = new int[20];
+
+        public void copy()
+        {
+            for (int i = 0; i < _oldArray.Length; i++)
+            {
+                _newArray[i] = _oldArray[i];
+            }
+
+            _oldArray = _newArray;
+        }
+
+        //oldArray 表示当前存储元素的数组
+        //size 表示当前元素个数
+        public void add(int index, int e)
+        {
+            if (index > size || index < 0)
+            {
+                Console.WriteLine("位置不合法...");
+            }
+
+            //如果数组已经满了 就扩容
+            if (size >= _oldArray.Length)
+            {
+                // 扩容函数可参考代码1
+            }
+
+            for (int i = size - 1; i >= index; i--)
+            {
+                _oldArray[i + 1] = _oldArray[i];
+            }
+
+            //将数组elementData从位置index的所有元素往后移一位
+            // System.arraycopy(oldArray, index, oldArray, index + 1,size - index);
+
+            _oldArray[index] = e;
+
+            size++;
+        }
 
         public int Size()
         {
             return size;
         }
 
+        /*
         public void set(int index, int item)
         {
             if (index < 0 || index >= size)
@@ -43,5 +88,6 @@
                 items = copy;
             }
         }
+        */
     }
 }
---
+++ Algorithms-Csharp/linkedlist/LinkedListGeneric.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Algorithms_Csharp.linkedlist
+{
+    class LinkedListGeneric
+    {
+        static void printList(Node<string> head)
+        {
+            Node<String> current = head;
+            while (current != null)
+            {
+                Console.WriteLine(current.item);
+                current = current.next;
+            }
+        }
+
+        static void printListRev(Node<String> head)
+        {
+            //倒序遍历链表主要用了递归的思想
+            if (head != null)
+            {
+                printListRev(head.next);
+                Console.WriteLine(head.item);
+            }
+        }
+
+        // 单链表反转 主要是逐一改变两个节点间的链接关系来完成
+        static Node<String> revList(Node<String> head)
+        {
+
+            if (head == null)
+            {
+                return null;
+            }
+
+            Node<String> nodeResult = null;
+
+            Node<String> nodePre = null;
+            Node<String> current = head;
+
+            while (current != null)
+            {
+                Node<String> nodeNext = current.next;
+
+                if (nodeNext == null)
+                {
+                    nodeResult = current;
+                }
+
+                current.next = nodePre;
+                nodePre = current;
+                current = nodeNext;
+            }
+
+            return nodeResult;
+        }
+    }
+
+    class Node<E>
+    {
+
+        internal E item;
+        internal Node<E> next;
+
+        // 构造函数
+        Node(E element)
+        {
+            this.item = element;
+            this.next = null;
+        }
+    }
+}
